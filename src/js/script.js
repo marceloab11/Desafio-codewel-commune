df0c83b60e98d160d822ceb7be2e1e44ce07bc7e
Marcelo Augusto <157186068+marceloab11@users.noreply.github.com>
2024-03-01 01:25:39 -0300

    projeto responsivo

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('vasco');
+var menu = document.querySelector("#menu");
+var modal = document.querySelector("#modal");
+var overlay = document.querySelector('#overlay');
+menu.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,8 @@
-console.log('vasco')
+const menu = document.querySelector("#menu") as HTMLDivElement
+const modal = document.querySelector("#modal") as HTMLDivElement
+const overlay = document.querySelector('#overlay') as HTMLDivElement
+
+menu.addEventListener('click', ()=> {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+})
